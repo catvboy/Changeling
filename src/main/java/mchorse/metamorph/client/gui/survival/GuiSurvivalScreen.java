2c6c28c292bd4947719500f0a9ecaa0305c5e8ce
asanetargoss <asanetargoss@users.noreply.github.com>
2021-01-10 21:52:47 -0800

    Fix double tapping arrow keys causing morphing at end of the survival morphing list

--- src/main/java/mchorse/metamorph/client/gui/survival/GuiSurvivalScreen.java
+++ src/main/java/mchorse/metamorph/client/gui/survival/GuiSurvivalScreen.java
@@ -27,6 +27,8 @@ import net.minecraft.client.gui.Gui;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 
+import java.io.IOException;
+
 import org.lwjgl.input.Keyboard;
 
 /**
@@ -53,7 +55,8 @@ public class GuiSurvivalScreen extends GuiBase
     
     AbstractMorph lastMorphSelected = null;
     private static final int DOUBLE_CLICK_TIME_MS = 500;
-    private long lastClickTime = -DOUBLE_CLICK_TIME_MS - 1;
+    private long lastMorphSelectTime = -DOUBLE_CLICK_TIME_MS - 1;
+    private boolean isClicking = false;
 
     public GuiSurvivalScreen()
     {
@@ -161,22 +164,47 @@ public class GuiSurvivalScreen extends GuiBase
         checkCurrentMorph(currentMorph, this.morphs.getSelected());
     }
     
-    private void checkDoubleClick(AbstractMorph morph)
+    @Override
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException
+    {
+        isClicking = true;
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+    }
+    
+    @Override
+    protected void mouseReleased(int mouseX, int mouseY, int state)
+    {
+        isClicking = false;
+        super.mouseReleased(mouseX, mouseY, state);
+    }
+    
+    @Override
+    protected void mouseClickMove(int mouseX, int mouseY, int clickedMouseButton, long timeSinceLastClick)
+    {
+        isClicking = false;
+        super.mouseClickMove(mouseX, mouseY, clickedMouseButton, timeSinceLastClick);
+    }
+    
+    private void checkDoubleClickMorph(AbstractMorph morph)
     {
         if (morph == null)
         {
             return;
         }
+        if (!isClicking)
+        {
+            return;
+        }
         
-        long clickTime = Minecraft.getSystemTime();
-        long dt = clickTime - lastClickTime;
-        lastClickTime = clickTime;
+        long morphSelectTime = Minecraft.getSystemTime();
+        long dt = morphSelectTime - lastMorphSelectTime;
+        lastMorphSelectTime = morphSelectTime;
         if (dt > 0 && dt < DOUBLE_CLICK_TIME_MS && lastMorphSelected.equals(morph))
         {
             MorphAPI.selectMorph(morph);
             GuiUtils.playClick();
             // Prevent re-fires
-            lastClickTime = 0;
+            lastMorphSelectTime = 0;
             this.closeScreen();
         }
         lastMorphSelected = morph;
@@ -203,7 +231,7 @@ public class GuiSurvivalScreen extends GuiBase
             this.keybind.setKeybind(morph.keybind);
         }
 
-        checkDoubleClick(morph);
+        checkDoubleClickMorph(morph);
     }
 
     /**
